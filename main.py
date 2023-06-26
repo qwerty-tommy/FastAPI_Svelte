630c6c6bc8b6af28fb5f94d92aada3585160e786
qwertyou <chjw1346@gmail.com>
2023-06-27 08:54:20 +0900

    Add posting question, reply function

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from domain.answer import answer_router
+from domain.question import question_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",    # 또는 "http://localhost:5173"
+]
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요 파이보"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(question_router.router)
+app.include_router(answer_router.router)
