45d79e114445ad4aa0adbb369926b3a663987d61
qwertyou <chjw1346@gmail.com>
2023-06-28 07:56:05 +0900

    Make login with jwt

--- domain/user/user_crud.py
+++ domain/user/user_crud.py
@@ -21,4 +21,6 @@ def get_existing_user(db: Session, user_create: UserCreate):
     ).first()
 
 
+def get_user(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
 
